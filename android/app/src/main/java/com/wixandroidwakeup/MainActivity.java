75a3f8ed6704c784588e3eb4a7c0ce86c8ef66af
Yee Wong <ywonggit@gmail.com>
2017-10-10 12:06:30 +0100

    first time app store launch fix

--- android/app/src/main/java/com/wixandroidwakeup/MainActivity.java
+++ android/app/src/main/java/com/wixandroidwakeup/MainActivity.java
@@ -3,5 +3,19 @@ package com.wixandroidwakeup;
 import com.reactnativenavigation.controllers.SplashActivity;
 
 public class MainActivity extends SplashActivity {
-
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    // fix app reloads every time when launch from app store after first download
+    if (!isTaskRoot()
+      && getIntent().hasCategory(Intent.CATEGORY_LAUNCHER)
+      && getIntent().getAction() != null
+      && getIntent().getFlags() != 0
+      && getIntent().getFlags() != Intent.FLAG_ACTIVITY_NEW_TASK
+      && getIntent().getPackage() == null
+      && getIntent().getAction().equals(Intent.ACTION_MAIN)) {
+      finish();
+      return;
+    }
+  }
 }
